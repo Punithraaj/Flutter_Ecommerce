5a24863f2cd352361c7b8795eb15327e61bed6e5
punith raj <punithraj14@gmail.com>
2025-01-11 13:11:03 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/screens/splash_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter ECommerce',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        canvasColor: Colors.transparent,
+        primarySwatch: Colors.blue,
+        fontFamily: "Montserrat",
+      ),
+      home: SplashScreen()
+    );
+  }
+}
---
+++ lib/screens/splash_page.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+import '../utility.dart';
+import 'authentication/login_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  late Animation<double> opacity;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+        duration: Duration(milliseconds: 2500), vsync: this);
+    opacity = Tween<double>(begin: 1.0, end: 0.0).animate(controller)
+      ..addListener(() {
+        setState(() {});
+      });
+    controller.forward().then((_) {
+      navigationPage();
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  void navigationPage() {
+    Navigator.of(context)
+        .pushReplacement(MaterialPageRoute(builder: (_) => LoginPage()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("splash_background.jpg"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Container(
+        decoration: BoxDecoration(color: transparentYellow),
+        child: SafeArea(
+            child: Scaffold(
+              body: Column(
+                children: [
+                  Expanded(
+                    child: Opacity(
+                        opacity: opacity.value,
+                        child: Image.asset('logo.png')),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: RichText(
+                      text: TextSpan(
+                          style: TextStyle(color: Colors.black),
+                          children: [
+                            TextSpan(text: 'Powered by '),
+                            TextSpan(
+                                text: 'Flutter And Dart Academy',
+                                style: TextStyle(fontWeight: FontWeight.bold))
+                          ]),
+                    ),
+                  )
+                ],
+              ),
+            )
+        ),
+      ),
+    );
+  }
+}
